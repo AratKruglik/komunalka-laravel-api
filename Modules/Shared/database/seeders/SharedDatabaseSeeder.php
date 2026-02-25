861b5d3906bffc1ca377b87a624eeea802bd3a39
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2026-02-25 17:31:45 +0200

    chore: Add UtilityTypeSeeder and CurrencySeeder to SharedDatabaseSeeder

--- Modules/Shared/database/seeders/SharedDatabaseSeeder.php
+++ Modules/Shared/database/seeders/SharedDatabaseSeeder.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Shared\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
 class SharedDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // $this->call([]);
+        $this->call([
+            UtilityTypeSeeder::class,
+            CurrencySeeder::class,
+        ]);
     }
 }
